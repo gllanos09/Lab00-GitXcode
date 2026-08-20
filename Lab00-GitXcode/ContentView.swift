835ec0f8db6d7bdb8b026d64a21a9511be3bf52d
Gabriel Llanos <angel.llanos@tecsup.edu.pe>
2026-08-20 12:15:27 -0500

    feat: Agregar vista de bienvenida con icono Swift

--- Lab00-GitXcode/ContentView.swift
+++ Lab00-GitXcode/ContentView.swift
@@ -2,18 +2,26 @@
 //  ContentView.swift
 //  Lab00-GitXcode
 //
-//  Created by Tecsup on 20/08/26.
+//  Created by Gabriel on 20/08/26.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        VStack(spacing: 20) {
+            Image(systemName: "swift")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.orange)
+                .font(.system(size: 60))
+            
+            Text("¡Hola, iOS Avanzado!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("Mi primer commit desde Xcode")
+                .font(.subheadline)
+                .foregroundStyle(.gray)
         }
         .padding()
     }
